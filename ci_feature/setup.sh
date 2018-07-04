f593aaa617beed356201737a323f411390810078
rashmichandrashekar <rashmy@microsoft.com>
2018-07-04 15:26:47 -0700

    update setup.sh
    writing logs to file

--- ci_feature/setup.sh
+++ ci_feature/setup.sh
@@ -5,7 +5,7 @@ wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent
 
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
-https://rashmi.blob.core.windows.net/bld-docker-image-1-31-pm/docker-cimprov-1.0.0-33.universal.x86_64.sh
+https://rashmi.blob.core.windows.net/bld-docker-image-3-21-pm/docker-cimprov-1.0.0-33.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
 #Extract omsbundle
