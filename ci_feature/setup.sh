b0fe8638d118bbd374fc2c5c0374006878729608
rashmichandrashekar <rashmy@microsoft.com>
2018-07-17 17:27:45 -0700

    moving try block up

--- ci_feature/setup.sh
+++ ci_feature/setup.sh
@@ -5,7 +5,7 @@ wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent
 
 #create file to disable omi service startup script
 touch /etc/.omi_disable_service_control
-wget https://rashmi.blob.core.windows.net/bld-dcoker-image-july17-12-14pm-ex-parsejson/docker-cimprov-2.0.0-4.universal.x86_64.sh
+wget https://rashmi.blob.core.windows.net/bld-dcoker-image-july17-17-17-move-try-up/docker-cimprov-2.0.0-4.universal.x86_64.sh
 chmod 775 $TMPDIR/*.sh
 
 #Extract omsbundle
